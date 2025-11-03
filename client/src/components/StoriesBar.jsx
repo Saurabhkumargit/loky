b3755f7401ef6540f65e476e41e348830c6bcabe
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-11-03 12:57:50 +0530

    view story

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -3,6 +3,7 @@ import { dummyStoriesData } from '../assets/assets';
 import { Plus } from 'lucide-react';
 import moment from 'moment';
 import StoryModal from './StoryModal';
+import StoryViewer from './StoryViewer';
 
 const StoriesBar = () => {
 
@@ -35,6 +36,7 @@ const StoriesBar = () => {
         {/* stories cards */}
         {stories.map((story, index) => (
           <div
+            onClick={()=> setViewStory(story)}
             key={index}
             className={`relative rounded-lg shadow min-w-30 max-w-30 amx-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}
           >
@@ -72,7 +74,8 @@ const StoriesBar = () => {
 
       {/* Add story modal */}
       {showModal && <StoryModal setShowModal={setShowModal} fetchStories={fetchStories} />}
-
+      {/* View Story Modal */}
+      {viewStory && <StoryViewer viewStory={viewStory} setViewStory={setViewStory} />}
     </div>
   );
 }
