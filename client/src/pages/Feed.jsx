201bfb4401281ed42ecc08156a90b8dff03f3e92
Saurabhkumargit <saurabhkumar.t21@gmail.com>
2025-11-12 17:58:10 +0530

    posts

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { dummyPostsData } from '../assets/assets';
 import Loading from '../components/Loading';
 import StoriesBar from '../components/StoriesBar';
+import PostCard from '../components/PostCard';
+
 
 const Feed = () => {
 
@@ -22,7 +24,11 @@ const Feed = () => {
       {/* stories and posts will go here */}
       <div>
         <StoriesBar />
-        <div className="p-4 space-y-6">List of posts</div>
+        <div className="p-4 space-y-6">
+          {feeds.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+          </div>
       </div>
 
       {/* Right sidebar */}
